869051862bff28f6ea67aaa10a71facc1b344453
eabrawer <ericbrawer@gmail.com>
2014-04-03 12:04:10 -0400

    Testing if world will respond to cols and rows attributes

--- game_of_life_spec.rb
+++ game_of_life_spec.rb
@@ -3,8 +3,14 @@ require_relative  './game_of_life.rb'
 describe "Game of Life" do
 	describe World do
 		subject { World.new }
+
 		it "should create a new world object" do
 			expect(subject).to be_a World
+		end
+
+		it "should respond to col and row methods" do
+			expect(subject).to respond_to(:cols)
+			expect(subject).to respond_to(:rows)			
 		end
 	end
 end
